76643094115c816a906917c720ec02d6fd5b9fdc
masterome1234 <jerome.alvez@adamson.edu.ph>
2021-04-08 13:26:14 +0800

    initial commit

---
+++ sample.php
@@ -0,0 +1,32 @@
+<?php
+define('NAME','Yoshi'); //function
+$a = 100; //variable declaration
+$b = 200;
+$c = $a+$b; //operations in php
+$name = "variable";
+$stringOne = 'My name is';
+$stringTwo = 'Jerome alvez';
+
+
+echo $name," \r\n"; //output 
+echo $c," \r\n";
+echo "hello"," \r\n";//sting output
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>my first php</title>
+</head>
+<body>
+<h1><?php echo'hell world';?></h1> 
+<div><?php echo'hell world';?></div>
+<div><?php echo NAME;?></div>
+<div><?php echo $stringOne . $stringTwo;?></div>
+<div><?php echo $name[1];
+echo strlen($name);
+echo strtoupper($name);
+echo strtolower($name);
+?></div>
+</body>
+</html>
